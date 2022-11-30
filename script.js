c9a78eb7a8e3bdc22fc41bf5a92407cf59ac98c6
David Mesoma <mesogod2006@gmail.com>
2022-11-30 21:07:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const playBtn = document.querySelector('#play');
 const nextBtn = document.querySelector('#next');
 const menu = document.querySelector('#menu');
 const musicCover = document.querySelector('#cover');
+const repeatBtn = document.querySelector('#repeat');
+const listBtn = document.querySelector('#list');
 
 const songs = ['Bad_Liar - Imagine_Dragons', 'What About Us - Pink', 'Natalie_Taylor - Surrender' ,'Fight Song - Rachel Platten', 'Unstoppable - Sia', 'Meghan-Trainor - \nBetter-When-Im-Dancin', 'Shape Of You - Ed Sheeran', 'Pentatonix - \nThats-Christmas-To-Me', 'Pentatonix - \nJoy_To_The_World', 'Sebastian_Yatra - \nDos_Oruguitas'];
 
@@ -62,6 +64,16 @@ function nextSong() {
     playSong();
 }
 
+function repeatSong() {
+    if (songIndex >= songIndex) {
+        repSongIndex = songIndex *= 1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
 function updateProgress(e) {
     const { duration, currentTime } = e.srcElement;
     const progressPercent = (currentTime / duration) * 100;
@@ -87,12 +99,38 @@ function playPause() {
     }
 }
 
+repeatBtn.addEventListener('click', repeatStyle);
+
+function repeatStyle() {
+    repeat.style.backgroundColor = 'rgb(255, 166, 0)';
+    repeatBtn.style.color = '#00ffa6';
+    repeat.style.padding = '5px';
+}
+
+repeatBtn.addEventListener('click', repeatSong);
+
 previousBtn.addEventListener('click', prevSong);
 
+previousBtn.addEventListener('click', () => {
+    repeatBtn.style.backgroundColor = null;
+    repeatBtn.style.padding = '0px';
+});
+
 nextBtn.addEventListener('click', nextSong);
+
+nextBtn.addEventListener('click', () => {
+    repeatBtn.style.backgroundColor = null;
+    repeatBtn.style.padding = '0px';
+});
 
 audio.addEventListener('timeupdate', updateProgress);
 
 progressContainer.addEventListener('click', setProgress);
 
-audio.addEventListener('ended', nextSong);
+audio.addEventListener('ended', () => {
+    if (songIndex == repSongIndex) {
+        repeatSong();
+    } else {
+        nextSong();
+    }
+});
